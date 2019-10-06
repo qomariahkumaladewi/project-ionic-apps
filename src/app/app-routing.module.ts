d5087e6eeb138108eeab291dccb565b411d4dbcc
Qomariah Kumala Dewi <qomariahkumaladewi9329@gmail.com>
2019-10-06 19:20:35 +0700

    challenge #2

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then(m => m.ListPageModule)
+  },  { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
+  { path: 'keranjang', loadChildren: './keranjang/keranjang.module#KeranjangPageModule' },
+  { path: 'tambah', loadChildren: './tambah/tambah.module#TambahPageModule' }
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
